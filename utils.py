f37aefc92e98e3f48c3739c81fd50ac663445ba1
WEGMANN Nathan <nathan.wegmann@unifr.ch>
2025-03-31 10:05:20 +0200

    refactored utils.py

--- utils.py
+++ utils.py
@@ -3,17 +3,21 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 
+def folders_to_csv(parent_folder_path, nb_folders, output_path):
+    data = []
+    for i in range(0,nb_folders):
+        folder_path = f"{parent_folder_path}{i}"
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            img = Image.open(file_path).convert("L")
+            vector = np.array(img).flatten()
+            vector = np.append(vector, i)
+            data.append(vector)
+    df = pd.DataFrame(data)
+    df.to_csv(output_path)
 
-data = []
-for i in range(0,10):
-    folder_path = f"data/test/{i}"
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        img = Image.open(file_path).convert("L")
-        vector = np.array(img).flatten()
-        vector = np.append(vector, i)
-        data.append(vector)
+folders_to_csv("data/test/", 10, "data/test.csv")
+folders_to_csv("data/train/", 10, "data/train.csv")
 
-df = pd.DataFrame(data)
-df.to_csv("data/mnist.csv")
-        
+
+            
