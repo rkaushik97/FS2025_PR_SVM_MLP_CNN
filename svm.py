41e4e100dbb11ba1cea3b6ac5f934e58b7589c20
WEGMANN Nathan <nathan.wegmann@unifr.ch>
2025-04-04 15:42:04 +0200

    SVM: Improve Accuracy: 0.9148 -> 0.9553

--- svm.py
+++ svm.py
@@ -1,10 +1,23 @@
 from sklearn.svm import SVC, LinearSVC
+from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay
 from sklearn.datasets import make_classification
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV, train_test_split
 import matplotlib.pyplot as plt
 import pandas as pd
 import joblib
+
+scaler = StandardScaler()
+pca = PCA(n_components=0.95)
+
+pipe = Pipeline([
+    ('scaler', StandardScaler()),
+    ('pca', PCA(n_components=0.95)),  # Or a fixed number like 50 or 100
+    ('svm', SVC(kernel='rbf'))  # You can try 'linear' or tune kernel
+])
+
 
 def prepare_data(data_path):
     df = pd.read_csv(data_path)
@@ -12,9 +25,12 @@ def prepare_data(data_path):
     y = df.iloc[:,-1]
     return X, y
 
-
 def find_best_params(data_path, param_grid = None, train_size = 0.2):
     X_train, y_train = prepare_data(data_path= data_path)
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_train = pca.fit_transform(X_train)
+
     X_tune, _, y_tune, _ = train_test_split(X_train, y_train, train_size = train_size, stratify=y_train, random_state=42)
     if param_grid == None:
         param_grid = {
@@ -26,23 +42,32 @@ def find_best_params(data_path, param_grid = None, train_size = 0.2):
     grid.fit(X_tune, y_tune)
     print("Best parameters:", grid.best_params_)
 
-
 def test_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
     print("Accuracy:", accuracy_score(y_test, y_pred))
     ConfusionMatrixDisplay.from_estimator(model, X_test, y_test, cmap='Blues')
     plt.show()
 
+
 #find_best_params("data/train.csv")
-#Best parameters: {'C': 0.1, 'gamma': 0.01, 'kernel': 'linear'}
+#Best parameters: {'C': 10, 'gamma': 0.001, 'kernel': 'rbf'}
 
 X_train, y_train = prepare_data("data/train.csv")
 X_test, y_test = prepare_data("data/test.csv")
+svm_optim = SVC(C= 10, gamma= 0.001, kernel= 'rbf')
 
 
-#X_smaller, _, y_smaller, _ = train_test_split(X_train, y_train, train_size = 0, stratify=y_train, random_state=42)
-#model = SVC(C= 0.1, kernel = 'linear')
-#model.fit(X_train, y_train)
-#print("finished_training")
-#joblib.dump(model, "svm_model.joblib")
-# Accuracy: 0.9148, could do better
+X_smaller, _, y_smaller, _ = train_test_split(X_train, y_train, train_size = 0.2, stratify=y_train, random_state=42)
+
+
+
+X_smaller = pca.fit_transform(scaler.fit_transform(X_smaller))
+X_test = pca.transform(scaler.transform(X_test))
+model = SVC(C= 10, kernel = 'rbf', gamma= 0.001)
+
+print("_training")
+model.fit(X_smaller, y_smaller)
+print("finished_training")
+joblib.dump(model, "svm_model.joblib")
+test_model(model, X_test=X_test, y_test=y_test)
+#SVM: Improve Accuracy: 0.9148 -> 0.9553
